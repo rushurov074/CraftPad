afdd58098bbec6f417662db00cc1deab4dc77c70
Ruslan Ushurov <ruslanushurov@Ruslans-MacBook-Pro.local>
2026-08-16 22:04:59 -0400

    updated firmware

--- Firmware/keymaps/default/keymap.c
+++ Firmware/keymaps/default/keymap.c
@@ -1,31 +1,32 @@
-#include QMK_KEYBOARD_H
+#include "quantum.h"
 
-//Create custom names for my two macro keys
 enum custom_keycodes {
-    MACRO_BLUESTACKS = SAFE_RANGE,
-    MACRO_DISCORD
+    MACRO_DISCORD = SAFE_RANGE,
+    MACRO_BLUESTACKS,
+    MACRO_MC_JAVA
 };
 
-//Program exactly what those macros do when pressed
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
+            case MACRO_DISCORD:
+                SEND_STRING(SS_LGUI(" ") SS_DELAY(200) "Discord" SS_DELAY(200) SS_TAP(X_ENTER));
+                return false;
             case MACRO_BLUESTACKS:
-                // Opens Spotlight, waits 200ms, types the app name, hits Enter
                 SEND_STRING(SS_LGUI(" ") SS_DELAY(200) "Bluestacks Air" SS_DELAY(200) SS_TAP(X_ENTER));
                 return false;
-            case MACRO_DISCORD:
-                // Opens Spotlight, waits 200ms, types the app name, hits Enter
-                SEND_STRING(SS_LGUI(" ") SS_DELAY(200) "Discord" SS_DELAY(200) SS_TAP(X_ENTER));
+            case MACRO_MC_JAVA:
+                SEND_STRING(SS_LGUI(" ") SS_DELAY(200) "Minecraft" SS_DELAY(200) SS_TAP(X_ENTER));
                 return false;
         }
     }
     return true;
-};
+}
 
-//Assign the macros and the Play/Pause key to my physical layout
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        MACRO_BLUESTACKS, MACRO_DISCORD, KC_MPLY
+        MACRO_DISCORD,   MACRO_BLUESTACKS, KC_NO,
+        MACRO_MC_JAVA,   KC_NO,            KC_NO,
+        KC_NO,           KC_NO,            KC_NO
     )
 };
